087d2bc101875e34bd413319fbd33dfb0ff14f13
Bintang <itzbintangyosua@gmail.com>
2024-02-10 15:08:17 +0700

    added destinations page

---
+++ src/data/destinations.ts
@@ -0,0 +1,67 @@
+type destination = {
+  name: string;
+  description: string;
+  slug: string;
+  imgURL: string;
+};
+
+const destinations: destination[] = [
+  {
+    name: "KOMODO ISLAND",
+    description:
+      "Komodo Island is one of the islands located in NTT. Komodo Island is known as the natural habitat of the Komodo dragon. This island is one of the Komodo National Park areas which is managed by the Central Government.",
+    imgURL: "/images/abimanyu-photowork-.webp",
+    slug: "komodo-island",
+  },
+  {
+    name: "RIUNG 17 ISLAND",
+    description:
+      "This marine park presents a different side of Flores Island. When setting foot in this marine park area which has more than seventeen islands, visitors are invited to directly experience the silence and beauty of the 17 Riung Island Marine Park.",
+    imgURL: "images/riung-17-island-marine.jpg",
+    slug: "riung-17-island",
+  },
+  {
+    name: "KELIMUTU LAKE",
+    description:
+      'Kelimutu is a combination of the words "keli" which means mountain and the word "mutu" which means to boil. According to local beliefs, the colors in Kelimutu Lake have their own meanings and have very powerful natural powers.',
+    imgURL: "/images/1672033802-dji-0222-jpg.jpg",
+    slug: "kelimutu-lake",
+  },
+  {
+    name: "PINK BEACH",
+    description:
+      "As the name suggests, this exotic and beautiful beach has pink sand. Pink Beach, also known as Red Beach on Komodo Island, is one of seven light red sand beaches in the world.",
+    imgURL: "/images/pink-beach.jpeg",
+    slug: "pink-beach",
+  },
+  {
+    name: "WAE REBO VILLAGE",
+    description:
+      "The traditional village of Wae Rebo in the district of Manggarai on the island of Flores, East Nusatenggara, has received the Top Award of Excellence from UNESCO in the 2012 UNESCO Asia Pacific Heritage Awards, announced in Bangkok on 27 August 2012.",
+    imgURL: "/images/wae rebo village.webp",
+    slug: "wae-rebo-village",
+  },
+  {
+    name: "PADAR ISLAND",
+    description:
+      "Speaking of nature, exotism and satisfaction, this island offers them all. Although you need a little bit of effort on the journey, the end result would be so much worth it. You wouldn’t regret the breathtaking view when you first arrive. As mentioned earlier, hiking to the peak of the hill lets you find out why it is called exotic.",
+    imgURL: "/images/fajruddin-mudzakkir-Padar-islan.webp",
+    slug: "padar-island",
+  },
+  {
+    name: "KOKA BEACH",
+    description:
+      "Koka beach is a beach which is very attractive with clean sea views, as well as the water is very clear, soft white sand stretches forming segment, left and right there are cliffs like a natural painting looks very artistic. Like the other southern coast, Koka beach has big waves but quite safe if we want to swim on the beach.",
+    imgURL: "/images/koka beach.jpg",
+    slug: "koka-beach",
+  },
+  {
+    name: "KANAWA ISLAND",
+    description:
+      "Kanawa Island is a small island that has crystal clear sea water with white sand and very beautiful coral reefs. This island has gradations of blue sea water and is very suitable as a tourist spot to relieve fatigue due to busy work.",
+    imgURL: "/images/kanawa island.webp",
+    slug: "kanawa-island",
+  },
+];
+
+export { destinations };
