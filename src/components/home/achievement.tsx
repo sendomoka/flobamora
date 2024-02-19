9a52127a09a9a5b222ed3ed206948330a0d64ca5
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-02-19 13:07:30 +0700

    feat: update UI achievement

--- src/components/home/achievement.tsx
+++ src/components/home/achievement.tsx
@@ -1,122 +1,134 @@
-import komodo from "/images/komodo_dragon_6_by_swordsdragon_dejec0t-fullview.png";
+import komodo from "/images/achievement/komodo.png";
 import { motion } from "framer-motion";
-import animationData from "../../components/lotties/animation.json";
-import Lottie from "react-lottie";
-import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { useEffect, useState } from "react";
 
 export function Achievement() {
+  const { t } = useTranslation();
   const achievements = [
     {
-      title: "1st Best in Travel 2020 Competition",
+      title: t("1st Best in Travel 2020 Competition"),
+      desc: t("The Ministry of Tourism and Creative Economy of the Republic of Indonesia has won the 1st Best in Travel 2020 competition in the category of Best Destination for Sustainable Tourism Development."),
       link: "https://www.youtube.com/watch?v=yG3RowE35sA",
     },
     {
-      title: "Wae Rebo Village - UNECSO World Heritage Site 2012",
+      title: t("Wae Rebo Village - UNECSO World Heritage Site 2012"),
+      desc: t("Wae Rebo Village is a traditional village located in the Manggarai Regency, East Nusa Tenggara. It is a UNESCO World Heritage Site 2012."),
       link: "https://www.visitsoutheastasia.travel/activity/unesco-top-award-of-excellence-2012-for-wae-rebos-mbaru-niang-traditional-houses-on-flores/",
     },
     {
-      title: "Komodo National Park - UNESCO World Heritage Site 1991",
+      title: t("Komodo National Park - UNESCO World Heritage Site 1991"),
+      desc: t("Komodo National Park is a national park in Indonesia located within the Lesser Sunda Islands in the border region between the provinces of East Nusa Tenggara and West Nusa Tenggara."),
       link: "https://en.unesco.org/biosphere/aspac/komodo",
     },
     {
-      title: "Pink Beach - 50 Best Beaches in the World 2019",
+      title: t("Pink Beach - 50 Best Beaches in the World 2019"),
+      desc: t("Pink Beach is one of the best beaches in the world. It is located in Komodo National Park, East Nusa Tenggara."),
       link: "https://en.tempo.co/read/1830832/indonesias-pink-beach-on-komodo-island-among-worlds-best-beaches",
     },
   ];
-
-  interface Position {
-    x: number;
-    y: number;
-  }
-
-  const [initialPositions, setInitialPositions] = useState<Position[]>([]);
-
+  const [leafTlX, setleafTlX] = useState('-26rem');
+  const [leafTlY, setleafTlY] = useState('9rem');
+  const [earthTlX, setearthTlX] = useState('-13rem');
+  const [earthTlY, setearthTlY] = useState('26rem');
   useEffect(() => {
-    const positions: { x: number; y: number }[] = [];
-    achievements.forEach(() => {
-      positions.push({
-        x: Math.random() * 100,
-        y: Math.random() * 100,
-      });
-    });
-
-    setInitialPositions(positions);
+    function handleResize() {
+      if (window.innerWidth < 640) {
+        setleafTlX('-20rem');
+        setleafTlY('6rem');
+        setearthTlX('-8rem');
+        setearthTlY('20rem');
+      } else {
+        setleafTlX('-26rem');
+        setleafTlY('9rem');
+        setearthTlX('-13rem');
+        setearthTlY('26rem');
+      }
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
-
-  const animateDirection = () => {
-    const directions = ["left", "right", "up", "down"];
-    return directions[Math.floor(Math.random() * directions.length)];
-  };
-
-  const transitionDuration = () => {
-    return Math.random() * 2 + 1;
-  };
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return (
-    <div className="flex flex-col h-screen lg:flex-row lg:justify-between lg:items-end">
-      <img
-        src={komodo}
-        alt="komodo"
-        className="sm:w-1/2 h-fit translate-y-[32rem] lg:translate-y-0"
-      />
-      <div className="p-6 -mt-44 sm:-mt-20 lg:mt-40 lg:w-1/2 lg:m-auto">
-        <div className="inline-flex items-center justify-center">
-          <h1
-            className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl"
-            style={{
-              fontFamily: "Dela Gothic One",
-            }}>
-            Achievement
-          </h1>
-          <div className=" absolute translate-x-[9.5rem] sm:translate-x-44 bs:translate-x-44 md:translate-x-56 xl:translate-x-[17rem] size-24 sm:size-36 xl:size-40">
-            <Lottie
-              options={defaultOptions}
-              isStopped={false}
-              isPaused={false}
-            />
-          </div>
-        </div>
-        <p className="my-4 text-justify opacity-60 sm:text-left">
-          Nusa Tenggara Timur's magnificent beaches, lakes, wildlife, and surf
-          breaks have brought it global attention as an eco-tourism hotspot. The
-          province continues to earn accolades for its sustainability
-          initiatives and wealth of natural attractions.
+    <div class="h-screen flex flex-col lg:flex-row justify-center items-center mt-96 md:mt-[30rem] lg:mt-10">
+      <div class="lg:w-1/2 flex justify-end lg:-translate-x-10 xl:-translate-x-32 scale-75 md:scale-100 lg:scale-75 xl:scale-100">
+        <img src={komodo} alt="komodo" class="w-[30rem] h-fit z-0" />
+        <motion.div
+          style={{
+            position: "absolute",
+            width: "7rem",
+            zIndex: 10,
+            // translateX: "-13rem",
+            // translateY: "26rem"
+            translateX: earthTlX,
+            translateY: earthTlY
+          }}
+          animate={{ rotate: [0, 10, -10, 10, 0] }}
+          transition={{ duration: 2, repeat: Infinity }}
+        >
+          <img src={t("/images/achievement/earth-en.png")} alt="earth" />
+        </motion.div>
+        <motion.div
+          style={{
+            position: "absolute",
+            width: "7rem",
+            zIndex: 10,
+            // translateX: "-26rem",
+            // translateY: "9rem"
+            translateX: leafTlX,
+            translateY: leafTlY
+          }}
+          animate={{ rotate: [0, 10, -10, 10, 0] }}
+          transition={{ duration: 2, repeat: Infinity }}
+        >
+          <img src={t("/images/achievement/leaf-en.png")} alt="leaf" />
+        </motion.div>
+        <motion.div
+          style={{
+            position: "absolute",
+            width: "7rem",
+            zIndex: 10,
+            translateX: "-5rem",
+            translateY: "-2rem"
+          }}
+          animate={{ rotate: [0, 10, -10, 10, 0] }}
+          transition={{ duration: 2, repeat: Infinity }}
+        >
+          <img src={t("/images/achievement/wave-en.png")} alt="wave" />
+        </motion.div>
+      </div>
+      <div class="lg:w-1/2 pt-10 md:pt-32 lg:pt-0 pb-96 lg:pb-0 px-4 md:px-10 lg:px-0 lg:pr-20 xl:pr-0">
+        <h1
+          className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl"
+          style={{
+            fontFamily: "Dela Gothic One",
+          }}>
+          {t("Achievement")}
+        </h1>
+        <p className="my-4 text-justify opacity-60 sm:text-left xl:w-[40rem]">
+          {t("East Nusa Tenggara's magnificent beaches, nature, wildlife, and surf breaks have brought it global attention as an eco-tourism hotspot. We continues to earn accolades for its sustainability initiatives and wealth of natural attractions.")}
         </p>
-        <div className="mt-10 sm:ml-80 md:ml-[22rem] lg:ml-20 sm:mt-20">
-          <motion.div>
-            {achievements.map((achievement, index) => (
-              <motion.div
-                key={index}
-                initial={initialPositions[index]}
-                animate={{
-                  x: animateDirection() === "left" ? -50 : 50,
-                  y: animateDirection() === "up" ? -50 : 50,
-                }}
-                transition={{
-                  duration: transitionDuration(),
-                  repeat: Infinity,
-                  repeatType: "mirror",
-                }}
-                // @ts-expect-error
-                className="p-2 mb-2 text-xs text-white transition-colors bg-green-800 rounded-md cursor-pointer w-fit dark:bg-green-950 hover:bg-green-700 hover:dark:bg-green-700">
-                <a
-                  href={achievement.link}
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  {achievement.title}
+        <Accordion type="single" collapsible className="xl:w-[40rem] opacity-80">
+          {achievements.map((achievement, index) => (
+            <AccordionItem key={index} value={`item-${index}`}>
+              <AccordionTrigger>
+                <b class="text-left">{achievement.title}</b>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p>{achievement.desc}</p>
+                <a href={achievement.link} target="_blank" rel="noreferrer" class="text-emerald-500 hover:underline">
+                  {t("More")}
                 </a>
-              </motion.div>
-            ))}
-          </motion.div>
-        </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </div>
   );
