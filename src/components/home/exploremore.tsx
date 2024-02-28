a811174e64677bd0dc164296e2f5cb835110ccee
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-02-28 21:10:57 +0700

    refactor: Update destinations UI & add primereact

--- src/components/home/exploremore.tsx
+++ src/components/home/exploremore.tsx
@@ -1,20 +1,52 @@
-import Flicking from "@egjs/preact-flicking";
-import "@egjs/preact-flicking/dist/flicking.css";
-import "@egjs/preact-flicking/dist/flicking-inline.css";
-import { destinations } from "@/data/destinations";
+import { DestinationService } from "@/data/destinations";
 import { useTranslation } from "react-i18next";
-import { Button } from "@/components/ui/button"
-import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
-import { useRef } from "react"
+import { useEffect, useState } from "react"
+import { Carousel, CarouselResponsiveOption } from 'primereact/carousel'
+
+interface Destination {
+  name: string;
+  imgURL: string;
+  mapLink: string;
+}
 
 export default function ExploreMore() {
   const { t } = useTranslation();
-  const flicking = useRef<Flicking | null>(null);
-  function handlePrevClick() {
-    (flicking.current as Flicking)?.prev();
-  }
-  function handleNextClick() {
-    (flicking.current as Flicking)?.next();    
+  const [destinations, setDestinations] = useState<Destination[]>([]);
+  const responsiveOptions: CarouselResponsiveOption[] = [
+    {
+      breakpoint: '1400px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '1199px',
+      numVisible: 3,
+      numScroll: 1
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '575px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ]
+  useEffect(() => {
+    DestinationService.map((value) => {
+      setDestinations((destinations) => [...destinations, value]);
+    })
+  }, [])
+  const destTemplate = (destination: Destination) => {
+    return (
+      <a href={destination.mapLink}>
+        <div className={"group w-[300px] bg-cover bg-no-repeat h-[600px] flex items-end rounded-3xl hover:grayscale lg:ml-6"} style={{ backgroundImage: `url('${destination.imgURL}')`, backgroundPositionX: "center" }} role={"presentation"}>
+          <h3 className={"text-5xl p-6 font-bold transition-transform duration-300 group-hover:-translate-y-8"}>{t(destination.name)}</h3>
+        </div>
+      </a>
+    )
   }
   return (
     <div className={"flex flex-col px-10 xl:px-56 py-10 gap-10"}>
@@ -35,62 +67,8 @@ export default function ExploreMore() {
         </p>
       </div>
       <div className={"text-white"}>
-        <div class="flex justify-end gap-2 -translate-y-4">
-          <Button variant="outline" size="icon" onClick={handlePrevClick}>
-            <ChevronLeftIcon 
-            // @ts-expect-error
-            className="size-6" />
-          </Button>
-          <Button variant="outline" size="icon" onClick={handleNextClick}>
-            <ChevronRightIcon 
-            // @ts-expect-error
-            className="size-6" />
-          </Button>
-        </div>
-        <Flicking align={"prev"} ref={flicking}>
-          {destinations.map((value, key) => {
-            return (
-              <div className={"flex flex-col gap-8"}>
-                <CardDestination
-                  imgURL={value.imgURL}
-                  mapLink={""}
-                  name={value.name}
-                  key={key}
-                />
-              </div>
-            );
-          })}
-        </Flicking>
+        <Carousel value={destinations} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} autoplayInterval={10000} circular itemTemplate={destTemplate} />
       </div>
     </div>
-  );
-}
-
-function CardDestination({
-  name,
-  mapLink,
-  imgURL,
-}: {
-  name: string;
-  mapLink: string;
-  imgURL: string;
-}) {
-  const { t } = useTranslation();
-  return (
-    <a href={mapLink}>
-      <div
-        className={
-          "group w-[300px] bg-cover bg-no-repeat h-[600px] flex items-end rounded-3xl mr-6 hover:grayscale"
-        }
-        style={{
-          backgroundImage: `url('${imgURL}')`,
-          backgroundPositionX: "center",
-        }}
-        role={"presentation"}>
-        <h3 className={"text-5xl p-6 font-bold transition-transform duration-300 group-hover:-translate-y-8"}>
-          {t(name)}
-        </h3>
-      </div>
-    </a>
   );
 }
