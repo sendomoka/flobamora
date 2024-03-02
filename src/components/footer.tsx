1416ba1783b6c1f04041555de8a4ef2d432903c9
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-02 17:24:07 +0700

    feat: add social media icons in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,6 +6,7 @@ import { WeatherType, getWeather } from "@/lib/getWeather";
 import { getUserTime } from "@/lib/getTime";
 import "@fontsource/dela-gothic-one";
 import { useTranslation } from "react-i18next";
+import { SocialIcon } from 'react-social-icons'
 
 export default function Footer() {
   const [weather, setWeather] = useState<WeatherType>();
@@ -68,34 +69,35 @@ export default function Footer() {
         <div className={"p-4 flex flex-col gap-3"}>
           <h4 className={"font-bold text-lg"}>Flobamora</h4>
           <div>
-            <div><a href="/" rel="noopener" className="hover:text-green-main transition-all">{t("Home")}</a></div>
-            <div><a href="/articles" rel="noopener" className="hover:text-green-main transition-all">{t("Articles")}</a></div>
-            <div><a href="/accomodations" rel="noopener" className="hover:text-green-main transition-all">{t("Lodging & Accommodations")}</a></div>
+            <div><a href="/" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">{t("Home")}</a></div>
+            <div><a href="/articles" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">{t("Articles")}</a></div>
+            <div><a href="/accomodations" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">{t("Lodging & Accommodations")}</a></div>
           </div>
         </div>
         <div className={"p-4 flex flex-col gap-3"}>
           <h4 className={"font-bold text-lg"}>{t("Data Source")}</h4>
           <div>
-            <div><a href="https://www.detik.com/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Detik.com</a></div>
-            <div><a href="https://www.wikipedia.org/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Wikipedia</a></div>
-            <div><a href="https://www.indonesia.travel/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Indonesia Travel</a></div>
-            <div><a href="https://www.kompas.com/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Kompas.com</a></div>
-            <div><a href="https://www.tripadvisor.co.id/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Tripadvisor</a></div>
-            <div><a href="https://www.gotravelaindonesia.com/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">gotravela Indonesia</a></div>
+            <div><a href="https://www.detik.com/" target="_blank" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">Detik.com</a></div>
+            <div><a href="https://www.wikipedia.org/" target="_blank" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">Wikipedia</a></div>
+            <div><a href="https://www.indonesia.travel/" target="_blank" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">Indonesia Travel</a></div>
+            <div><a href="https://www.kompas.com/" target="_blank" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">Kompas.com</a></div>
+            <div><a href="https://www.tripadvisor.co.id/" target="_blank" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">Tripadvisor</a></div>
+            <div><a href="https://www.gotravelaindonesia.com/" target="_blank" rel="noopener" className="opacity-50 hover:opacity-100 transition-all">gotravela Indonesia</a></div>
           </div>
         </div>
         <div className={"p-4 flex flex-col gap-3"}>
           <h4 className={"font-bold text-lg"}>{t("About Us")}</h4>
-          <div>
-            <div><a href="https://www.instagram.com/jehianth/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Jehian Athaya Tsani Az Zuhry</a></div>
-            <div><a href="https://www.instagram.com/dzakonee/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Dzakwan Irfan Ramdhani</a></div>
-            <div><a href="https://www.instagram.com/itzbintangyosua/" target="_blank" rel="noopener" className="hover:text-green-main transition-all">Panky Bintang Pradana Yosua</a></div>
+          <div class="flex gap-2">
+            <SocialIcon network="x" className="hover:grayscale hover:translate-y-1 transition-all duration-300 cursor-pointer" />
+            <SocialIcon network="tiktok" className="hover:grayscale hover:translate-y-1 transition-all duration-300 cursor-pointer" />
+            <SocialIcon network="instagram" className="hover:grayscale hover:translate-y-1 transition-all duration-300 cursor-pointer" />
+            <SocialIcon network="youtube" className="hover:grayscale hover:translate-y-1 transition-all duration-300 cursor-pointer" />
           </div>
         </div>
       </div>
       <hr className={"w-full border-black"} />
-      <div className={"mx-auto text-center py-10"}>
-        Copyright 2024 Flobamora | All rights reserved
+      <div className={"mx-auto text-center text-sm text-neutral-400 py-10"}>
+        Copyright 2024 <a href="https://github.com/sendomoka/flobamora" class="hover:underline hover:text-white transition-all duration-300" >Flobamora</a>. All rights reserved.
       </div>
     </div>
   );
